422ef944caf5f00bbbecc9c2af71c78d19c14684
Robin Pindel <robin.pindel@gmail.com>
2023-01-04 23:07:20 -0600

    Adding module 1 assignment to repo

---
+++ FanInstances.java
@@ -0,0 +1,118 @@
+/*Robin Pindel
+ * Module 1 Fan Class Assignment
+ * 1/5/2023*/
+
+//ADD COMMENTS FOR EVERYTHING!!!
+
+
+//Public class of the file that will contain the main method for creating instances of Fan and 
+//executing code regarding these instances
+public class FanInstances{
+    //Main method
+    public static void main(String[] args){
+        //Create two instances of fan, one default and one custom
+        Fan defaultFan = new Fan();
+        Fan customFan = new Fan(3, true, 100, "pink");
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("The default-built fan's initial specifications are: ");   
+        System.out.println(defaultFan);
+        System.out.println("");
+        System.out.println("The custom-built fan's initial specifications are: ");
+        System.out.println(customFan);
+        System.out.println("");
+        System.out.println("");
+
+        System.out.println("Changing the default fan's on status to: ");
+        defaultFan.setOn(true);
+        System.out.println(defaultFan.getOn());
+
+        System.out.println("Changing the default fan's speed to: ");
+        defaultFan.setSpeed(1);
+        System.out.println(defaultFan.getSpeed());
+
+        System.out.println("Changing the custom fan's radius to (not sure how but...): ");
+        customFan.setRadius(3);
+        System.out.println(customFan.getRadius());
+        
+        System.out.println("Changing the custom fan's color to: ");
+        customFan.setColor("dirty");
+        System.out.println(customFan.getColor());
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("The default-built fan's current specifications are: ");   
+        System.out.println(defaultFan);
+        System.out.println("");
+        System.out.println("The custom-built fan's current specifications are: ");
+        System.out.println(customFan);
+        System.out.println("");
+        System.out.println("");
+
+
+
+    }
+}
+
+//Class that will serve as template for Fan instances
+class Fan{
+    static final int STOPPED = 0;
+    static final int SLOW = 1;
+    static final int MEDIUM = 2;
+    static final int FAST = 3;
+    private int speed = 0;
+    private boolean on;
+    private int radius = 6;
+    private String color = "white";
+
+
+    public Fan(){}
+
+    public Fan(int customSpeed, boolean customOn, int customRadius, String customColor){
+        speed = customSpeed;
+        on = customOn;
+        radius= customRadius;
+        color = customColor;
+    }
+
+
+    public void setSpeed(int setSpeed){
+        if(on == true){
+            speed = setSpeed;
+        }
+    }
+
+    public int getSpeed(){
+        return speed;
+    }
+
+    public void setOn(boolean setOn){
+        on = setOn;
+    }
+
+    public boolean getOn(){
+        return on;
+    }
+
+    public void setRadius(int setRadius){
+        radius = setRadius;
+    }
+
+    public int getRadius(){
+        return radius;
+    }
+
+    public void setColor(String setColor){
+        color = setColor;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public String toString(){
+        System.out.println();
+        return "On: " + on + ", Speed: " + speed + ", Radius: " + radius + ", Color: " + color;
+    }
+}
