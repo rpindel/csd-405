287a1a3372b026bba9e7e06cd5e6d6a742cbeb19
Robin Pindel <robin.pindel@gmail.com>
2023-01-20 18:06:31 -0600

    Starting Module 4 code

---
+++ module-4/PindelArrayListTest.java
@@ -0,0 +1,18 @@
+/* Robin Pindel
+ * Module 4 Assignment
+ * 1/21/2023 */
+
+import java.util.ArrayList;
+//import java.util.Scanner;
+
+public class PindelArrayListTest{
+    //Main method
+    public static void main(String[] args) {
+        //Creat ArrayList
+        //Populate ArrayList with user inputs until 0
+        //Process ArrayList for largest value
+    
+    public static Integer max(ArrayList list){}
+            //Display largest value to user
+    }
+}
