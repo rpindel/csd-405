33827600bca0a7d533b766787f94817da4fcc5d0
Robin Pindel <robin.pindel@gmail.com>
2023-01-28 11:40:11 -0600

    Submitting near final Module 5 assignments code

--- module-5/Mod5DataFileTesting.java
+++ module-5/Mod5DataFileTesting.java
@@ -8,29 +8,76 @@
 
  public class Mod5DataFileTesting {
     //main method
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         //create file data.file if it does not exist        
         try {
             File dataFile = new File("C:\\Users\\Coding\\csd\\csd-405\\module-5\\data.file");
-            boolean existance = dataFile.createNewFile();
             
-            if (existance){
+            if (dataFile.createNewFile()){
+                System.out.println("");
                 System.out.println("The new file \"data.file\" has been created!");
+                System.out.println("");
             }
             else {
-            System.out.println("The file \"data.file\" already exists!");
+                System.out.println("");
+                System.out.println("The file \"data.file\" already exists!  Any write work will " 
+                + "be in append mode.");
+                System.out.println("");
             }
         }
         catch (Exception e){
-            dataFile.printStackTrace();
+            System.out.println("An error has occured.  Please review the exception stack track " + 
+            "for more details.");
+            System.out.println("");
+            e.printStackTrace();
+            System.out.println("");
         }
 
         //write to data.file adding 10 randomly generated numbers or append to existing file
-
-
         //each int should be separated by a space
         //close data.file
+        try {
+            FileWriter dataFileWriter = new FileWriter("C:\\Users\\Coding\\csd\\csd-405\\" + 
+            "module-5\\data.file", true);
+            System.out.println("data.file is open for writing.");
+            System.out.println("Writing 10 random integers 1-100.");
+            for (int i = 0; i < 10; i++){
+                dataFileWriter.write(((int)(Math.random() * 100)) + " ");
+            }
+            dataFileWriter.close();
+            System.out.println("data.file is closed for writing.");
+            System.out.println("");
+        }
+        catch (Exception e){
+            System.out.println("An error has occured.  Please review the exception stack track " + 
+            "for more details.");
+            System.out.println("");
+            e.printStackTrace();
+            System.out.println("");
+        }
+
         //reopen data.file
         //read and display data.file
+        try {
+            File dataFile = new File("C:\\Users\\Coding\\csd\\csd-405\\" + 
+            "module-5\\data.file");
+            Scanner myReader = new Scanner(dataFile);
+            System.out.println("data.file is open for reading.");
+            while (myReader.hasNext()){
+                String data = myReader.nextLine();
+                System.out.println("data.file contains the following random numbers:");
+                System.out.println(data);
+            }
+            myReader.close();
+            System.out.println("data.file is closed for reading.");
+            System.out.println("");
+        }
+        catch (Exception e){
+            System.out.println("An error has occured.  Please review the exception stack track " + 
+            "for more details.");
+            System.out.println("");
+            e.printStackTrace();
+            System.out.println("");
+        }
     }
  }
--- module-5/Mod5OOBExceptionTesting.java
+++ module-5/Mod5OOBExceptionTesting.java
@@ -8,7 +8,10 @@ import java.util.*;
 public class Mod5OOBExceptionTesting {
     //main method
     public static void main(String[] args){
+        //create ArrayList
         ArrayList<String> arrayList = new ArrayList<>();
+        
+        //Populate ArrayList with Pokemon names
         arrayList.add("#1 Cyndaquil");
         arrayList.add("#2 Dodrio");
         arrayList.add("#3 Weedle");
@@ -20,16 +23,20 @@ public class Mod5OOBExceptionTesting {
         arrayList.add("#9 Horsea");
         arrayList.add("#10 Gengar");
     
+        //for-each loop to print out the ArrayList of Pokemon names
         for (String pokemon : arrayList)
             System.out.println(pokemon + " ");
         
+        //Scanner created for user input
         Scanner input = new Scanner(System.in);   
 
+        //Ask user which Pokemon name they would like to see again
         System.out.println("");
-        System.out.println("Which pokemon would you like to see again?  Please enter the number.");
+        System.out.println("Which Pokemon would you like to see again?  Please enter the number.");
         int intValue = input.nextInt();
         intValue = intValue - 1;
         
+        //Try-catch to handle exception where user selects a number not in the ArrayList
         try {
             System.out.println("");
             System.out.println(arrayList.get(intValue));
@@ -37,7 +44,7 @@ public class Mod5OOBExceptionTesting {
         } 
         catch(Exception e) {
             System.out.println("Out of Bounds: The number you have entered is not valid " + 
-            "for this pokemon list.");
+            "for this Pokemon list.");
             System.out.println("");
         }
         finally {
