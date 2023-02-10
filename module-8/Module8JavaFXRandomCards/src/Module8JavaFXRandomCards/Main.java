de1b787aef0e6f18636ca03bd65473115f24d9db
Robin Pindel <robin.pindel@gmail.com>
2023-02-09 23:19:23 -0600

    Start of Mod 8 code to repo

---
+++ module-8/Module8JavaFXRandomCards/src/Module8JavaFXRandomCards/Main.java
@@ -0,0 +1,24 @@
+/*Robin Pindel
+ * Module 8 JaxaFX Random Cards Assignment
+ * 2/9/2023
+ */
+package Module8JavaFXRandomCards;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+ 
+//Override the start method of the Application class
+    @Override
+    public void start(Stage primaryStage) {
+    }    
+    //Main method since I am using VSCode
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
