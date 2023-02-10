1431cac6b6ee254eb38a4a39c89c5474e7cd8d5d
Robin Pindel <robin.pindel@gmail.com>
2023-02-10 06:49:45 -0600

    Final code submission for Module 8 assignment

--- module-8/Module8JavaFXRandomCards/src/Module8JavaFXRandomCards/Module8JavaFXRandomCardsMain.java
+++ module-8/Module8JavaFXRandomCards/src/Module8JavaFXRandomCards/Module8JavaFXRandomCardsMain.java
@@ -1,11 +1,11 @@
 /*Robin Pindel
  * Module 8 JaxaFX Random Cards Assignment
- * 2/9/2023
+ * 2/10/2023
  * 
  * Reference
  * Borderpane (javafx 8). (2015, February 10). Retrieved February 9, 2023, 
  * from https://docs.oracle.com/javase/8/javafx/api/javafx/scene/layout/BorderPane.html
- * Page 441
+ * Pages 441 and 586
  */
 package Module8JavaFXRandomCards;
 
@@ -19,28 +19,27 @@ import javafx.scene.image.*;
 import java.util.*;
 
 
-public class Main extends Application {
+public class Module8JavaFXRandomCardsMain extends Application {
  
 //Override the start method of the Application class
     @Override
     public void start(Stage primaryStage) {
-    //Store numbers 1-52 to array list
+    
+    //Store numbers 1-52 to ArrayList
     ArrayList<Integer> cardList = new ArrayList<>();
     for (int i = 1; i <=52; i++){
         cardList.add(i);
     }
 
-    //Random shuffle of array list 11.12
-    //System.out.println(cardList);
+    //Random shuffle of ArrayList
     java.util.Collections.shuffle(cardList);
-    //System.out.println(cardList);
 
-    //Use first 4 numbers in array list [0-3] as the image file names for the cards
+    //Use first 4 numbers in ArrayList [0-3] as the image file names for the cards
     //Create an Image and ImageView for each selected card
-    Image card1 = new Image("C:\\Users\\Coding\\csd\\csd-405\\module-8\\cards\\" + cardList.get(0) + ".png");
-    Image card2 = new Image("C:\\Users\\Coding\\csd\\csd-405\\module-8\\cards\\" + cardList.get(1) + ".png");
-    Image card3 = new Image("C:\\Users\\Coding\\csd\\csd-405\\module-8\\cards\\" + cardList.get(2) + ".png");
-    Image card4 = new Image("C:\\Users\\Coding\\csd\\csd-405\\module-8\\cards\\" + cardList.get(3) + ".png");
+    Image card1 = new Image("file:rsc\\cards\\" + cardList.get(0) + ".png");
+    Image card2 = new Image("file:rsc\\cards\\" + cardList.get(1) + ".png");
+    Image card3 = new Image("file:rsc\\cards\\" + cardList.get(2) + ".png");
+    Image card4 = new Image("file:rsc\\cards\\" + cardList.get(3) + ".png");
     ImageView imageView1 = new ImageView(card1);
     ImageView imageView2 = new ImageView(card2);
     ImageView imageView3 = new ImageView(card3);
