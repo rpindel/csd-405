1446e54d873ecb832062a4efdb25ea65e32c61d2
Robin Pindel <robin.pindel@gmail.com>
2023-02-09 23:12:52 -0600

    Final code for Module 7 essay examples

--- module-7/MainJavaFXWorkspace/src/MainJavaFXWorkspace/MainBorderPane.java
+++ module-7/MainJavaFXWorkspace/src/MainJavaFXWorkspace/MainBorderPane.java
@@ -1,5 +1,6 @@
 /*Robin Pindel
  * Module 7 JaxaFX Examples for Essay
+ * 2/9/2023
  * Main.java for BorderPane
  */
 
@@ -13,21 +14,26 @@ import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class MainBorderPane extends Application {
-
+    //Override the start method of the Application class
     @Override
     public void start(Stage primaryStage) {
+        //Create a BorderPane
+        //Then create CustomPanes as defined in the extra non-public class
+        //Creating CustomPanes like our textbook (Page 564) allowed me to name and alter their style easier
         BorderPane bPane = new BorderPane();
         CustomPane cPDead = new CustomPane("Is Dead?");
         CustomPane cPMana = new CustomPane("Low Mana?");
         CustomPane cPPoisoned = new CustomPane("Is Poisoned?");
         CustomPane cPHealth = new CustomPane("Low Health?");
         
+        //Create final CustomePane and assign all CustomPanes to BorderPane regions
         bPane.setTop(new CustomPane("Character Name"));
         bPane.setRight(cPMana);
         bPane.setBottom(cPDead);
         bPane.setLeft(cPHealth);
         bPane.setCenter(cPPoisoned);
         
+        //Manual toggles for character status
         //boolean dead = false;
         //boolean lowMana = false;
         //boolean poisoned = false;
@@ -37,25 +43,32 @@ public class MainBorderPane extends Application {
         boolean poisoned = true;
         boolean lowHealth = true;
 
+        //Styles controls for BorderPane regions based on status variable boolean values
         if (dead == true) {
-            cPDead.setStyle("-fx-background-color: gray;" + "-fx-border-color: black;" + "-fx-font-weight: bold;");
+            cPDead.setStyle("-fx-background-color: gray;" + "-fx-border-color: black;" 
+            + "-fx-font-weight: bold;");
         }
         if (lowMana == true) {
-            cPMana.setStyle("-fx-background-color: aqua blue;" + "-fx-border-color: black;" + "-fx-font-weight: bold;");
+            cPMana.setStyle("-fx-background-color: aqua blue;" + "-fx-border-color: black;" 
+            + "-fx-font-weight: bold;");
         }
         if (poisoned == true) {
-            cPPoisoned.setStyle("-fx-background-color: purple;" + "-fx-border-color: black;" + "-fx-font-weight: bold;");
+            cPPoisoned.setStyle("-fx-background-color: purple;" + "-fx-border-color: black;" 
+            + "-fx-font-weight: bold;");
         }
         if (lowHealth == true) {
-            cPHealth.setStyle("-fx-background-color: red;" + "-fx-border-color: black;" + "-fx-font-weight: bold;");
+            cPHealth.setStyle("-fx-background-color: red;" + "-fx-border-color: black;" 
+            + "-fx-font-weight: bold;");
         }
 
+        //Create and set the scene in the stage then display the stage
         Scene scene = new Scene(bPane);
         primaryStage.setTitle("JavaFXTest");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    //Non-public class to create CustomPane nodes
     class CustomPane extends StackPane {
         public CustomPane(String title) {
             getChildren().add(new Label(title));
@@ -63,7 +76,7 @@ public class MainBorderPane extends Application {
             setPadding(new Insets(20, 20, 20, 20));
         }  
     }
-
+//Main method since I am using VSCode
 public static void main(String[] args) {
     Application.launch(args);
     }
--- module-7/MainJavaFXWorkspace/src/MainJavaFXWorkspace/MainHBox.java
+++ module-7/MainJavaFXWorkspace/src/MainJavaFXWorkspace/MainHBox.java
@@ -1,5 +1,6 @@
 /*Robin Pindel
  * Module 7 JaxaFX Examples for Essay
+ * 2/9/2023
  * Main.java for HBox
  */
 
@@ -16,35 +17,44 @@ import javafx.scene.text.*;
 import javafx.stage.Stage;
 
 public class MainHBox extends Application {
-
+    //Override the start method of the Application class
     @Override
     public void start(Stage primaryStage) {
+        //Create and set basic config for the HBox
+        //Then create a StackPane to use with the HBox
         HBox hbox = new HBox();
         hbox.setPadding(new Insets(10, 10, 10, 10));
         hbox.setAlignment(Pos.CENTER);
         StackPane stack = new StackPane();
         
+        //Create the children nodes that will be added to the HBox and StackPane
+        //These nodes will form the flag body, border, and label
         Rectangle r1 = new Rectangle(0, 0, 50, 100);
         Rectangle r2 = new Rectangle(50, 0, 50, 100);
         Rectangle r3 = new Rectangle(100, 0, 50, 100);
         Rectangle border = new Rectangle(0, 0, 150, 100);
         Text text = new Text("Ireland");
         
+        //Set color properties for the flag body Rectangles
+        //Then set color properties for the border Rectangle
         r1.setFill(Color.ORANGE);
         r2.setFill(Color.WHITE);
         r3.setFill(Color.GREEN);
         border.setStroke(Color.BLACK);
         border.setFill(null);
         
+        //Add flag body Rectangles to the HBox
+        //Then add the HBox, border Rectangle, and label to StackPane
         hbox.getChildren().addAll(r1, r2, r3);
         stack.getChildren().addAll(hbox, border, text);
 
+        //Create and set the scene in the stage then display the stage
         Scene scene = new Scene(stack, 220, 170);
         primaryStage.setTitle("JavaFXTest");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
+//Main method since I am using VSCode
 public static void main(String[] args) {
     Application.launch(args);
     }
