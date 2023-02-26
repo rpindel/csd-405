e44f06e1a3b1bea0806ac81ed2aca8c7148b9577
Robin Pindel <robin.pindel@gmail.com>
2023-02-26 09:23:29 -0600

    Mod 11 code framework

---
+++ module-11/Mod11ShapeRandomColor/src/Mod11ShapeRandomColor/Mod11ShapeRandomColorMain.java
@@ -0,0 +1,31 @@
+/*Robin Pindel
+ * Mod 11 Shape with Random Color Fill Assignment
+ * 2/26/2023
+ */
+
+package Mod11ShapeRandomColor;
+
+//Define imports
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.scene.image.*;
+import javafx.scene.control.*;
+import java.util.*;
+
+public class Mod11ShapeRandomColorMain extends Application {
+    //Override the start method of the Application class
+    @Override
+    public void start(Stage primaryStage) {
+    }
+    
+    
+    //Main method since I am using VSCode
+    public static void main(String[] args) {
+       Application.launch(args);
+    }
+
+}
